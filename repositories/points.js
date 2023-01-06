ca24d948cf4f2eb52d1b5b2a8f5ef69d34f3484f
Sergey Lubimov <siarhei_liubimau@epam.com>
2023-01-06 11:08:59 +0300

    feat: disable default logging & enable for 'points' queries

--- repositories/points.js
+++ repositories/points.js
@@ -1,10 +1,22 @@
 const db = require('../db/models/index');
 
+function logSQLstatementAndDuration(sql, queryObject) {
+    console.log('\033[33m%s\033[32m', sql, queryObject)
+  }
+
 module.exports = {
     getPoints: async() => {
-        return await db.point.findAll()
+        return await db.point.findAll({
+            benchmark: true,
+            logging: (sql, queryObject) => {
+              logSQLstatementAndDuration(sql, queryObject)
+            }})
     },
     addPoints: async (points) => {
-        return `Add Points:\n ${ await db.point.create(points) }`
+        return `Add Points:\n ${ await db.point.create(points, {
+            benchmark: true,
+            logging: (sql, queryObject) => {
+              logSQLstatementAndDuration(sql, queryObject)
+            }}) }`
       }
 }
